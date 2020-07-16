9cea616b4c9f375b727d8bc07d10d95b258f94da
tatsuya828828 <kkjy011894@gmail.com>
2020-07-16 11:00:14 +0900

    [finish]10-02

--- src/Main.java
+++ src/Main.java
@@ -2,24 +2,33 @@ import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Writer;
+import java.util.StringTokenizer;
 
 public class Main {
 	public static void main(String[] args) {
+		String s = "ミナト,アサカ,スガワラ";
+		// StringTokenizerクラスは、指定したデリミタで文字列分割して、それぞれのデータを順次取得する機能を提供する
+		StringTokenizer st = new StringTokenizer(s, ","); // カンマをデリミタ指定
+		// hasMoreTokenで次のトークンがあるか検査
+		while(st.hasMoreTokens()) {
+			// nextTokenで次のトークンを指定
+			String t = st.nextToken();
+			System.out.println(t);
+		}
 
+		// より厳密なCSVの操作には、opencsvやOrangeSignal CSVなどCSV処理専用の機能を持つライブラリを利用した方が良い
 	}
 
 	public void saveHeroToFile(Hero h) throws IOException {
 		Writer w = new BufferedWriter(new FileWriter("rpgsave.dat"));
-		// このデータの保存形式のルール
-		// ファイルには文字情報として勇者の情報を保存する
-		// 1行目には名前を、2行目にはHPを、そして3行目にMPを保存する。
-		// このように複数のデータをどのような形式・順序で保存するかと言うことを定めたルールのことをデータフォーマットという
-		// もしデータフォーマットに従わず順番を間違って読み込んでしまうと重大な不具合になる
-		// また、ファイル内のデータ構造を規定するものはファイルフォーマット
-		// ネットワーク経由で伝送される一塊りのメッセージのデータ構造を規定するものはメッセージフォーマットまたは、電文フォーマットと呼ばれている
-		w.write(h.name +"\n"); // 末尾に改行をつけて保存
-		w.write(h.hp +"\n");
-		w.write(h.mp +"\n");
+		// 複数のデータを間まで区切って順に格納するデータフォーマットの形式をCSV形式と呼ぶ
+		// CSV内でデータを区切るために使うもじはデリミタと呼ばれる
+		// デリミタには、通常カンマが使われるが、スペースやタブ文字などを使うこともできる
+		w.write(h.name);
+		w.write(",");
+		w.write(h.hp);
+		w.write(",");
+		w.write(h.mp);
 		w.flush();
 		w.close();
 	}
