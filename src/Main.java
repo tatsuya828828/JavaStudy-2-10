7f8710e6f16f67f9dffa941ed7dbcb557880d717
tatsuya828828 <kkjy011894@gmail.com>
2020-07-16 11:29:04 +0900

    [finish]10-03

--- src/Main.java
+++ src/Main.java
@@ -1,35 +1,43 @@
-import java.io.BufferedWriter;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.Reader;
 import java.io.Writer;
-import java.util.StringTokenizer;
+import java.util.Properties;
 
 public class Main {
 	public static void main(String[] args) {
-		String s = "ミナト,アサカ,スガワラ";
-		// StringTokenizerクラスは、指定したデリミタで文字列分割して、それぞれのデータを順次取得する機能を提供する
-		StringTokenizer st = new StringTokenizer(s, ","); // カンマをデリミタ指定
-		// hasMoreTokenで次のトークンがあるか検査
-		while(st.hasMoreTokens()) {
-			// nextTokenで次のトークンを指定
-			String t = st.nextToken();
-			System.out.println(t);
+		// プロパティファイル形式の書き込み
+		// Propertiesクラスは、全てのデータを文字列として扱う
+		// 各行にはキーと値をペアとして、それらをペアデリミタで区切って記述する
+		// ペアデリミタは、イコール、コロン、空白のいずれかの文字
+		// シャープまたは感嘆符(!)で始まる行はコメントとして無視される
+		// ファイルの拡張子には.propertiesが用いられるといったルールなどがある
+
+		try(Writer fw = new FileWriter("rpgsave.properties")){
+			Properties p = new Properties();
+			p.setProperty("heroName", "アサカ"); // データのセット
+			p.setProperty("heroHp", "62");
+			p.setProperty("heroMp", "45");
+			// setPropertyメソッドは呼び出すだけでファイルには書き込まないので必ずstoreメソッドで保存を行う
+			p.store(fw, "勇者の情報"); // ファイルへ書きだす、引数はファイルの先頭にコメントとして出力される
+		} catch(IOException e) {
+			System.out.println("セーブに失敗しました");
+			e.printStackTrace();
 		}
 
-		// より厳密なCSVの操作には、opencsvやOrangeSignal CSVなどCSV処理専用の機能を持つライブラリを利用した方が良い
-	}
-
-	public void saveHeroToFile(Hero h) throws IOException {
-		Writer w = new BufferedWriter(new FileWriter("rpgsave.dat"));
-		// 複数のデータを間まで区切って順に格納するデータフォーマットの形式をCSV形式と呼ぶ
-		// CSV内でデータを区切るために使うもじはデリミタと呼ばれる
-		// デリミタには、通常カンマが使われるが、スペースやタブ文字などを使うこともできる
-		w.write(h.name);
-		w.write(",");
-		w.write(h.hp);
-		w.write(",");
-		w.write(h.mp);
-		w.flush();
-		w.close();
+		// プロパティファイル形式の読み込み
+		try(Reader fr = new FileReader("rpgsave.properties")){
+			Properties p2 = new Properties();
+			p2.load(fr); // ファイルの内容を読み込む
+			String name = p2.getProperty("heroName"); // キーを指定し取り出す
+			String strHp = p2.getProperty("heroHp");
+			int hp = Integer.parseInt(strHp);
+			System.out.println("勇者の名前:"+ name);
+			System.out.println("勇者のHP:"+ hp);
+		} catch(IOException e) {
+			System.out.println("読み込みに失敗しました");
+			e.printStackTrace();
+		}
 	}
 }
